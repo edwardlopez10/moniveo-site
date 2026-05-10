d6c2926db78ec22748e1d88e137369f50424d0fa
Edward <edward.lopez.ma@gmail.com>
2026-05-09 23:41:37 -0500

    Migrating to latest Next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,5 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import { FlatCompat } from "@eslint/eslintrc";
-import { globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
+import coreWebVitals from "eslint-config-next/core-web-vitals";
+import typescript from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-export default [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-];
+export default defineConfig([...coreWebVitals, ...typescript]);
